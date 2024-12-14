1bf312de731f51ac053867780a84b44d9a236e4a
lenabojc81 <lena.bojc@student.um.si>
2024-12-14 16:58:19 +0100

    changes

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+
+Object.defineProperty(window, "location", {
+    writable: true,
+    value: {
+        ...window.location,
+        reload: jest.fn(),
+    },
+});
